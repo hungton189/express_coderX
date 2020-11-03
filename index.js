806645b6710c39879c001b55adfbdfb07acafc0d
Hungton <hungton987@gmail.com>
2020-11-03 20:45:25 +0700

    add template engines pug

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', './views')
+
 app.listen(3000,function(){
     console.log("server is listening on port "+port)
 });
 
 app.get("/",(req, res)=>
 {
-    res.send('hello express')
+    res.render('index',
+    {
+        name:"abc"
+    })
+});
+
+app.get("/user",(req, res)=>
+{
+    res.render('users/index',
+    {
+        users:
+        [
+            {id:1, name:"Hưng tôn"},
+            {id:2, name:"tôn thất hưng"}
+        ]
+    })
 });
