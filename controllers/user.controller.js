11cf2ed46bbdf799be5f29f9289fe0ddb5a93ff1
Hungton <hungton987@gmail.com>
2020-11-05 11:45:25 +0700

    using controller(MVC)

---
+++ controllers/user.controller.js
@@ -0,0 +1,48 @@
+const shortid = require("shortid");
+var db = require('../db');
+
+module.exports.index = (req, res)=>
+{
+    res.render('users/index',
+    {
+        users:db.get("users").value()
+    })
+}
+
+module.exports.search = (req,res)=>
+{
+    const q = req.query.q;
+    const users = db.get("users").value();
+    const matchedUsers = users.filter(user=>
+        {
+            return user.name.toLowerCase().indexOf(q.toLowerCase()) !==-1;
+        });
+    res.render('users/index',
+        {
+            users:matchedUsers,
+            query:q
+        })
+}
+
+module.exports.create = (req,res)=>
+{
+    res.render('users/createUser');
+}
+
+module.exports.viewUser = (req, res)=>
+{
+    const id=req.params.id;
+    const user = db.get("users").find({id:id}).value();
+    res.render('users/view',
+    {
+        name:user.name
+    });
+}
+
+module.exports.postCreate = (req,res)=>
+{
+    const id = shortid.generate();
+    req.body.id = id;
+    db.get("users").push(req.body).write();
+    res.redirect('/user');
+}
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,53 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const shortid = require("shortid");
-var db = require('../db');
 
+var userController = require("../controllers/user.controller");
 
-router.get("/",(req, res)=>
-{
-    res.render('users/index',
-    {
-        users:db.get("users").value()
-    })
-});
+router.get("/",userController.index);
 
-router.get("/search",(req,res)=>
-{
-    const q = req.query.q;
-    const users = db.get("users").value();
-    const matchedUsers = users.filter(user=>
-        {
-            return user.name.toLowerCase().indexOf(q.toLowerCase()) !==-1;
-        });
-    res.render('users/index',
-        {
-            users:matchedUsers,
-            query:q
-        })
-});
+router.get("/search",userController.search);
 
-router.get('/create',(req,res)=>
-{
-    res.render('users/createUser');
-});
+router.get('/create',userController.create);
 
-router.get('/:id',(req, res)=>
-{
-    const id=req.params.id;
-    const user = db.get("users").find({id:id}).value();
-    res.render('users/view',
-    {
-        name:user.name
-    });
-});
+router.get('/:id',userController.viewUser);
 
-router.post('/create',(req,res)=>
-{
-    const id = shortid.generate();
-    req.body.id = id;
-    db.get("users").push(req.body).write();
-    res.redirect('/user');
-});
+router.post('/create',userController.postCreate);
 
 module.exports = router;
