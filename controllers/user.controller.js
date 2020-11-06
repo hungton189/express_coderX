c2528a2e3e806190e544ead5ea654d9ad7b9a42a
tôn hưng <hungton987@example.com>
2020-11-06 17:44:49 +0700

    validate CREATE USER

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -30,7 +30,7 @@ module.exports.viewUser = (req, res)=>
     const user = db.get("users").find({id:id}).value();
     res.render('users/view',
     {
-        name:user.name
+        user:user
     });
 }
 
@@ -43,6 +43,24 @@ module.exports.create = (req,res)=>
 module.exports.postCreate = (req,res)=>
 {
     const id = shortid.generate();
+    let errors = [];
+    if(!req.body.name)
+    {
+        errors.push("Chưa nhập name.");
+    }
+    if(!req.body.phone)
+    {
+        errors.push("Chưa nhập phone.");
+    }
+    if(errors.length)
+    {
+        res.render('users/createUser',
+        {
+            errors:errors,
+            values:req.body
+        });
+        return;
+    }
     req.body.id = id;
     db.get("users").push(req.body).write();
     res.redirect('/user');
