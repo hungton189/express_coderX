fc0601f5db8678baaf2d2b3a677ffc58de81cea5
tôn hưng <hungton987@example.com>
2020-11-09 17:25:37 +0700

    using PROCESS.ENV

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 
@@ -15,7 +17,7 @@ app.set('views', './views');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) 
 // for parsing application/x-www-form-urlencoded
-app.use(cookieParser("sdjbfjbfjdsafbaj"))
+app.use(cookieParser(process.env.SESSION_SECRET))
 
 app.use(express.static('public'))
 
